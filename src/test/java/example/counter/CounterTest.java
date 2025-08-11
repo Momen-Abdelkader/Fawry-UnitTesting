f6384290906dc0db0d4a1fb399b2ac3d1d29f915
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2025-08-12 00:23:38 +0300

    [RED] test(Counter): should accumulate values

--- src/test/java/example/counter/CounterTest.java
+++ src/test/java/example/counter/CounterTest.java
@@ -4,9 +4,18 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CounterTest {
+
     @Test
     void shouldStartAtZero() {
         Counter counter = new Counter();
         assertThat(counter.getValue()).isEqualTo(0);
+    }
+
+    @Test
+    void shouldAccumulateValues() {
+        Counter counter = new Counter();
+        counter.add(5);
+        counter.add(3);
+        assertThat(counter.getValue()).isEqualTo(8);
     }
 }
