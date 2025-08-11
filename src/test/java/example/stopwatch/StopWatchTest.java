a64875cefe438052a9d9076c575feb14bc2c9c8d
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2025-08-12 00:46:41 +0300

    [RED] test(StopWatch): should record minutes

---
+++ src/test/java/example/stopwatch/StopWatchTest.java
@@ -0,0 +1,13 @@
+package example.stopwatch;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class StopWatchTest {
+    @Test
+    void shouldRecordMinutes() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.record(10);
+        assertThat(stopWatch.getMinutes()).isEqualTo(10);
+    }
+}
