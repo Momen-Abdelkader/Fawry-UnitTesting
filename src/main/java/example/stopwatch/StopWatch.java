083de1ddbacef950dc5c6ad26c475dd487743eab
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2025-08-12 00:52:54 +0300

    [GREEN] feat(StopWatch): passed should ignore negative input test

--- src/main/java/example/stopwatch/StopWatch.java
+++ src/main/java/example/stopwatch/StopWatch.java
@@ -4,6 +4,10 @@ public class StopWatch {
     private int minutes = 0;
 
     public void recordMinutes(int minutes) {
+        if (minutes < 0) {
+            return;
+        }
+
         this.minutes += minutes;
     }
 
