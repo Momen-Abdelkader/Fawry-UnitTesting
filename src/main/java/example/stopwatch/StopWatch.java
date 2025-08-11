6fb4fac26cdb4affc4d909ab9837e57089271b97
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2025-08-12 01:05:14 +0300

    [GREEN] feat(StopWatch): passed should record days when hours exceed 24 test

--- src/main/java/example/stopwatch/StopWatch.java
+++ src/main/java/example/stopwatch/StopWatch.java
@@ -3,6 +3,7 @@ package example.stopwatch;
 public class StopWatch {
     private int minutes = 0;
     private int hours = 0;
+    private int days = 0;
 
     public void recordMinutes(int minutesToRecord) {
         if (minutesToRecord < 0) {
@@ -14,6 +15,11 @@ public class StopWatch {
             hours = minutes / 60;
             minutes -= 60 * hours;
         }
+
+        if (hours > 24) {
+            days = hours / 24;
+            hours -= 24 * days;
+        }
     }
 
     public int getMinutes() {
@@ -22,5 +28,9 @@ public class StopWatch {
 
     public int getHours() {
         return hours;
+    }
+
+    public int getDays() {
+        return days;
     }
 }
