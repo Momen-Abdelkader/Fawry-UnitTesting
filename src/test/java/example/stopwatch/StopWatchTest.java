546262026cf7085878c89f1e4d5314bf4330fb83
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2025-08-12 01:42:45 +0300

    [REFACTOR] refactor(StopWatch): added some comments

--- src/test/java/example/stopwatch/StopWatchTest.java
+++ src/test/java/example/stopwatch/StopWatchTest.java
@@ -21,47 +21,74 @@ public class StopWatchTest {
 
     @Test
     void shouldRecordMinutes() {
+        // Arrange/Act
         stopWatchUnderTest.recordMinutes(10);
+
+        // Assert
         assertTime(10, 0, 0);
     }
 
     @Test
     void shouldIgnoreNegativeInput() {
+        // Arrange/Act
         stopWatchUnderTest.recordMinutes(-10);
+
+        // Assert
         assertTime(0, 0, 0);
     }
 
     @Test
     void shouldRecordHoursWhenMinutesExceed60() {
+        // Arrange/Act
         stopWatchUnderTest.recordMinutes(130);
+
+        // Assert
         assertTime(10, 2, 0);
     }
 
     @Test
     void shouldRecordDaysWhenHoursExceed24() {
+        // Arrange/Act
         stopWatchUnderTest.recordMinutes(3010);
+
+        // Assert
         assertTime(10, 2, 2);
     }
 
     @Test
     void shouldBeAbleToRecordDaysBasedOnWorkHours() {
+        // Arrange
         stopWatchUnderTest.setWorkingHours(true);
+
+        // Act
         stopWatchUnderTest.recordMinutes(3010);
+
+        // Assert
         assertTime(10, 2, 6);
     }
 
     @Test
     void shouldConvertFullDaysToWorkingDays() {
+        // Arrange
         stopWatchUnderTest.recordMinutes(3010);
+
+        // Act
         stopWatchUnderTest.setWorkingHours(true);
+
+        // Assert
         assertTime(10, 2, 6);
     }
 
     @Test
     void shouldConvertWorkingDaysToFullDays() {
+        // Arrange
         stopWatchUnderTest.setWorkingHours(true);
         stopWatchUnderTest.recordMinutes(3010);
+
+        // Act
         stopWatchUnderTest.setWorkingHours(false);
+
+        // Assert
         assertTime(10, 2, 2);
     }
 }
